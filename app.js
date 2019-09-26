d9d7520b3bc9a595ac9fba87fba556f4750f86bb
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2019-09-26 15:53:34 +0200

    Actually included Webhooks to send the Message with nice formatting

--- app.js
+++ app.js
@@ -6,27 +6,56 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content.startsWith('!sponge')) {
-        var sentence = msg.content.replace('!sponge ','').toLowerCase();
-        var sponge_text = "";
-        var size = Math.random() < 0.5;
-        for(char of sentence){
-            if(size){
-                if(char === 'ß'){
-                    sponge_text += 'ẞ';
-                } else {
-                    sponge_text += char.toUpperCase();
-                }
-                size = false;
-            }else{
-                sponge_text += char;
-                size = true;
+function createMockingText(sentence){
+    var mockingText = "";
+    var size = Math.random() < 0.5;
+    for(char of sentence){
+        if(size){
+            if(char === 'ß'){
+                mockingText += 'ẞ';
+            } else {
+                mockingText += char.toUpperCase();
             }
+            size = false;
+        }else{
+            mockingText += char;
+            size = true;
         }
-        //msg.edit(sponge_text);
-        //TODO Replace user message with spongebob text
-        msg.reply(sponge_text);
+    }
+    return mockingText;
+}
+
+client.on('message', msg => {
+    content = msg.content;
+    channel = msg.channel;
+    user = msg.author;
+    //TODO Replace !sponge with bot mention
+    if (content.startsWith('!sponge')) {
+        console.log('Trying to fetch webhooks...');
+        hook = channel.fetchWebhooks()
+            .then(webhooks => {
+                console.log('Checking existing Webhooks');
+                let hook;
+                for(webhook of webhooks.values()){
+                    if(webhook.owner.id == client.user.id){
+                        console.log('Found valid Webhook');
+                        hook = webhook;
+                    }
+                }
+                if(hook==undefined){
+                    console.log('Creating new Webhook');
+                    //TODO Have to wait until webhook actually got created before using it
+                    hook = channel.createWebhook('Example Webhook', 'https://i.imgur.com/p2qNFag.png');
+                }
+                console.log('Trying to send message via webhook');
+                var mockingText = createMockingText(content.replace('!sponge ','').toLowerCase());
+                hook.send(mockingText, {
+                    username:user.username,
+                    avatarURL:user.avatarURL
+                });
+                msg.delete();
+            }
+        ).catch(console.error);
     }
 });
 
