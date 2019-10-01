39d5124b310008dbc3e1cb47f2af2a42d21664f5
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2019-10-01 15:50:52 +0200

    Updated webhook image

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.TOKEN;
 
+const botPic = 'https://cdn.discordapp.com/avatars/626697323057119244/dcefdb851b9cd88cc9cd19d6275cdbb8.png';
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -25,7 +27,7 @@ function createMockingText(sentence){
     return mockingText;
 }
 
-function sendMockingText(text,user,webhook){
+function sendWebhookText(text,user,webhook){
     console.log('Trying to send message via webhook');
     return webhook.send(text, {
         username:user.username,
@@ -41,25 +43,33 @@ client.on('message', msg => {
     if (content.startsWith('!sponge')) {
         var mockingText = createMockingText(content.replace('!sponge ','').toLowerCase());
         console.log('Trying to fetch webhooks...');
-        hook = channel.fetchWebhooks()
-            .then(webhooks => {
-                console.log('Checking existing Webhooks');
-                let hook;
-                for(webhook of webhooks.values()){
-                    if(webhook.owner.id == client.user.id){
-                        console.log('Found valid Webhook');
-                        sendMockingText(mockingText,user,webhook).then(msg.delete());
-                        return;
-                    }
+        channel.fetchWebhooks().then(webhooks => {
+            console.log('Checking existing Webhooks');
+            let hook;
+            for(webhook of webhooks.values()){
+                if(webhook.owner.id == client.user.id){
+                    console.log('Found valid Webhook');
+                    sendWebhookText(mockingText,user,webhook).then(msg.delete());
+                    return;
                 }
-
-                console.log('Creating new Webhook');
-                channel.createWebhook('SpongeBot', 'https://i.imgur.com/p2qNFag.png')
-                    .then(webhook => sendMockingText(mockingText,user,webhook))
-                    .then(msg.delete()
-                );
             }
-        ).catch(console.error);
+
+            console.log('Creating new Webhook');
+            channel.createWebhook('SpongeBot', botPic)
+                .then(webhook => sendWebhookText(mockingText,user,webhook))
+                .then(msg.delete()
+            );
+        }).catch(console.error);
+    }else if(content.startsWith('!really')){
+        //TODO Hier den text vom user lesen
+        //TODO Danach 3 Nachrichten verschicken
+        //TODO Erste Nachricht im Namen des Users
+        // Ich würde gerne [action]
+        //TODO Zweite Nachricht vom Bot selber
+        // Wirklich?
+        // TODO Dritte Nachricht im Namen des Users
+        // Nein...
+        // TODO Command selber wieder löschen
     }
 });
 
