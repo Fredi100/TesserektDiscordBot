d0deb9ad60a3a1d454a6ea021067e825ff233fc2
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2020-03-03 13:36:16 +0100

    Integrated MeshinBot into TesserektBot

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const TOKEN = process.env.TOKEN;
@@ -10,19 +11,46 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+//
+// MeshinBot
+//
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function parseBuzzwordsJson() {
+    var contents = fs.readFileSync('buzzwords.json');
+    return JSON.parse(contents);
+}
+
+function getRandomBuzzword(){
+    var jsonContent = parseBuzzwordsJson();
+
+    var randomNumber = getRandomInt(0,jsonContent.amount);
+
+    return jsonContent.buzzwords[randomNumber];
+}
+
+//
+// SpongeBot
+//
+
 // Takes a sentance as a String and mocks it
-function createMockingText(sentence){
+function createMockingText(sentence) {
     var mockingText = "";
     var size = Math.random() < 0.5;
-    for(char of sentence){
-        if(size){
-            if(char === 'ß'){
+    for (char of sentence) {
+        if (size) {
+            if (char === 'ß') {
                 mockingText += 'ẞ';
             } else {
                 mockingText += char.toUpperCase();
             }
             size = false;
-        }else{
+        } else {
             mockingText += char;
             size = true;
         }
@@ -31,59 +59,65 @@ function createMockingText(sentence){
 }
 
 // Takes a text, the user the bot should impersonate and the given webhook to send it to
-function sendWebhookText(text,user,webhook){
+function sendWebhookText(text, user, webhook) {
     console.log('Trying to send message via webhook');
     return webhook.send(text, {
-        username:user.username,
-        avatarURL:user.avatarURL
+        username: user.username,
+        avatarURL: user.avatarURL
     });
 }
 
-async function commandReally(text,channel,user){
+async function commandReally(text, channel, user) {
     console.log('commandReally');
-    var action = text.replace('!really ','');
-    var webhook = await channel.createWebhook(botName,botPic);
-    await webhook.send('Ich würde gerne '+action,{username:user.username,avatarURL:user.avatarURL});
+    var action = text.replace('!really ', '');
+    var webhook = await channel.createWebhook(botName, botPic);
+    await webhook.send('Ich würde gerne ' + action, { username: user.username, avatarURL: user.avatarURL });
     await channel.send('Wirklich?');
-    await webhook.send('Nö',{username:user.username,avatarURL:user.avatarURL});
+    await webhook.send('Nö', { username: user.username, avatarURL: user.avatarURL });
     webhook.delete('Not needed');
 }
 
-async function commandMock(text,channel,user){
+async function commandMock(text, channel, user) {
     console.log('commandMock');
-    var mockingText = createMockingText(text.replace('!sponge ','').toLowerCase());
-    var webhook = await channel.createWebhook(botName,botPic)
-    await sendWebhookText(mockingText,user,webhook);
+    var mockingText = createMockingText(text.replace('!sponge ', '').toLowerCase());
+    var webhook = await channel.createWebhook(botName, botPic)
+    await sendWebhookText(mockingText, user, webhook);
     webhook.delete();
 }
 
-async function hashtagTechnology(channel){
+async function hashtagTechnology(channel) {
     console.log('hashtagTechnology');
-    channel.send('',{files: ['./media/technology.gif']});
+    channel.send('', { files: ['./media/technology.gif'] });
 }
 
-async function hashtagFuture(channel){
+async function hashtagFuture(channel) {
     console.log('hashtagFuture');
-    channel.send('',{files: ['./media/future.gif']});
+    channel.send('', { files: ['./media/future.gif'] });
 }
 
-async function hashtagImagination(channel){
+async function hashtagImagination(channel) {
     console.log('hashtagImagination');
-    channel.send('',{files: ['./media/imagination.gif']});
+    channel.send('', { files: ['./media/imagination.gif'] });
 }
 
 client.on('message', msg => {
+    if(msg.content === "!meshin"){
+        var text = parseBuzzwordsJson().text;
+        var buzzword = getRandomBuzzword();
+
+        msg.channel.send(text.concat("\n",buzzword));
+    }
     if (msg.content.startsWith('!sponge')) {
-        commandMock(msg.content,msg.channel,msg.author);
+        commandMock(msg.content, msg.channel, msg.author);
         msg.delete();
-    }else if(msg.content.startsWith('!really')){
-        commandReally(msg.content,msg.channel,msg.author);
+    } else if (msg.content.startsWith('!really')) {
+        commandReally(msg.content, msg.channel, msg.author);
         msg.delete();
-    }else if(msg.content.endsWith('#technology')){
+    } else if (msg.content.endsWith('#technology')) {
         hashtagTechnology(msg.channel);
-    }else if(msg.content.endsWith('#future')){
+    } else if (msg.content.endsWith('#future')) {
         hashtagFuture(msg.channel);
-    }else if(msg.content.endsWith('#imagination')){
+    } else if (msg.content.endsWith('#imagination')) {
         hashtagImagination(msg.channel);
     }
 });
